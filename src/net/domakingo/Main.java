e4cfad30edb107f2f79886d70be8c691c5748e78
Domakingo <domakingtheking@gmail.com>
2023-01-13 23:31:14 +0100

    Input with scanner Output
    Trace: 5th playlist video of tutorials by Kaupenjoe
    Signed-off-by: Domakingo <domakingtheking@gmail.com>

--- src/net/domakingo/Main.java
+++ src/net/domakingo/Main.java
@@ -1,9 +1,24 @@
 package net.domakingo;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        // outputting things whit println
 
+        /*
+        System.out.println("Hello world!");
+        int x = 10;
+        System.out.println(x);
+        */
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Che giorno è oggi? ");
+        int input = scanner.nextInt();
+        int mese = 31;
+        int giorni_rimanenti = mese - input;
+        System.out.println("Mancano " + giorni_rimanenti + " giorni alla fine del mese");
 
     }
 }
